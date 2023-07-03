bc71024d8d9b894606546f33207acb382876a0f8
Thomas09-2 <138483646+Thomas09-2@users.noreply.github.com>
2023-07-03 13:39:11 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,14 @@
+let Temp = 0
+let Anzahl = 0
+while (Anzahl < 5) {
+    Anzahl += 1
+    basic.showNumber(Anzahl)
+}
+basic.forever(function () {
+    Temp = input.temperature()
+    if (Temp < 0) {
+        if (Temp < 0) {
+            basic.showNumber(Temp)
+        }
+    }
+})
